01a973feeac09971fd45f4f3dcf54bf0ef90a3ef
Raza ur Rehman <154697961+Raza181261@users.noreply.github.com>
2023-12-28 10:32:17 +0400

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,84 @@
+let shopItemData = [{
+    id : "itemid",
+    name:"Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "Img/img-1.jpg"
+}, 
+{
+    id : "itemid2",
+    name:"Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "Img/img-2.jpg"
+}, 
+{
+    id : "itemid3",
+    name:"T Shirt",
+    price: 25,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "Img/img-3.jpg"
+}, 
+{
+    id : "itemid4",
+    name:"Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "Img/img-4.jpg"
+},
+{
+    id: "itemid5",
+    name: "Mens Tie",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-5.png",
+  },
+  {
+    id: "itemid6",
+    name: "Casual shoes",
+    price: 200,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-6.png",
+  },
+  {
+    id: "itemid7",
+    name: "black suit",
+    price: 450,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-7.png",
+  },
+  {
+    id: "itemid8",
+    name: "polo shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-8.png",
+  },
+  {
+    id: "itemid9",
+    name: "denim shirt",
+    price: 85,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-9.png",
+  },
+  {
+    id: "itemid10",
+    name: "denim pants",
+    price: 120,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-10.png",
+  },
+  {
+    id: "itemid11",
+    name: "basic cap",
+    price: 35,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-11.png",
+  },
+  {
+    id: "itemid512",
+    name: "leather boots",
+    price: 350,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "Img/img-12.png",
+  },];
